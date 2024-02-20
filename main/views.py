5a08ca8ba1764e59bd391fc85884ac22b0e8d9bc
Vlad Kuguk <vladys.amigo97@gmail.com>
2024-02-20 13:56:30 +0100

    First commit

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        "title": "Home",
+        'content': 'Main page'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
