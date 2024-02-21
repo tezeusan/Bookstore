c6db0de304455ef49b7f3da24830d9c91f2ec96f
Vlad Kuguk <vladys.amigo97@gmail.com>
2024-02-21 11:40:48 +0100

    adding catalog and product pages

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
-from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-
+    path('', include("main.urls", namespace='main')),
+    path('catalog/', include("goods.urls", namespace='catalog'))
 ]
