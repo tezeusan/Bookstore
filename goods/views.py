482d9e0669ff0fe1539fdf7219f1004fbd63bb13
Vlad Kuguk <vladys.amigo97@gmail.com>
2024-03-04 12:57:14 +0100

    slug for books

--- goods/views.py
+++ goods/views.py
@@ -15,8 +15,8 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request, product_id):
-    product = Products.objects.get(id=product_id)
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
     context = {
         'product': product
     }
