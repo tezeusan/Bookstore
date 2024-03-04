31c401559b98531d550ba343be103a08156b0df4
Vlad Kuguk <vladys.amigo97@gmail.com>
2024-03-04 13:12:12 +0100

    slug for categories

--- goods/views.py
+++ goods/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from goods.models import Products
 
 
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+
 
     context = {
         "title": "Catalog",
