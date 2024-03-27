265825d9c0066eaf42ffa1eff7ee4845fa0bddb4
Vlad Kuguk <vladys.amigo97@gmail.com>
2024-03-27 20:11:58 +0100

    phone number validator

--- orders/forms.py
+++ orders/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 
 class CreateOrderForm(forms.Form):
-
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
@@ -10,12 +9,24 @@ class CreateOrderForm(forms.Form):
         choices=[
             ("0", False),
             ("1", True),
-            ],
-        )
+        ],
+    )
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(
         choices=[
             ("0", 'False'),
             ("1", 'True'),
-            ],
-        )
+        ],
+    )
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("The phone number must contain only digits.")
+
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Incorrect phone number format.")
+
+        return data
