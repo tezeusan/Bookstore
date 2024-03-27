ce1c53b9625c5de70d78fc1e4f90f9dfc02efc45
Vlad Kuguk <vladys.amigo97@gmail.com>
2024-03-27 17:02:16 +0100

    Created order template and form filled

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -25,9 +25,8 @@ urlpatterns = [
     path('', include("main.urls", namespace='main')),
     path('catalog/', include("goods.urls", namespace='catalog')),
     path('user/', include("users.urls", namespace='user')),
-    path('carts/', include('carts.urls', namespace='carts')),
-
-
+    path('cart/', include('carts.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:
--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='avatar')
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
 
     class Meta:
         db_table = "user"
